5cd89d427256101582cec33fde80703a7f7fdc2b
Lunny Xiao <xiaolunwen@gmail.com>
2019-06-27 16:46:17 +0800

    support local sql log

--- engine.go
+++ engine.go
@@ -329,7 +329,7 @@ func (engine *Engine) Ping() error {
 	return session.Ping()
 }
 
-// logging sql
+// logSQL save sql
 func (engine *Engine) logSQL(sqlStr string, sqlArgs ...interface{}) {
 	if engine.showSQL && !engine.showExecTime {
 		if len(sqlArgs) > 0 {
